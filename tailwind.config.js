0371cbee04e60935734f4743cd4a7735295a2962
Sefat-Khan <149791936+Sefat-Khan@users.noreply.github.com>
2023-11-23 09:44:28 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+       colors: {
+        'dark-blue' : '#00001a',
+        'light-orange' : '#f15d51'
+       },
+
+       fontFamily: {
+         'inter' : 'Inter'
+       }
+       
+    },
+  },
+  plugins: [],
+}
+
